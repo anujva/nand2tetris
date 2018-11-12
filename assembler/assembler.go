6d354e9e09bb762b54d3ce0b0066de15cb2fc7a0
Anuj Varma <anujva@gmail.com>
2018-11-11 17:42:31 -0800

    Removed the extra parser file

--- assembler/assembler.go
+++ assembler/assembler.go
@@ -11,13 +11,11 @@ type HackAssembler struct {
 
 //New returns a pointer to an object of HackAssembler
 func New() *HackAssembler {
-	immutableSymbolTable := initializeSymbolTable()
 	return &HackAssembler{}
 }
 
 //NewWithFile returns a HackAssemblers output file specified
 func NewWithFile(file *os.File) *HackAssembler {
-	immutableSymbolTable := initializeSymbolTable()
 	return &HackAssembler{
 		outputfile: file,
 	}
@@ -26,4 +24,11 @@ func NewWithFile(file *os.File) *HackAssembler {
 //SetOutputFile set the output file for the assembler
 func (ha *HackAssembler) SetOutputFile(file *os.File) {
 	ha.outputfile = file
+}
+
+// What do I need an assembler to do? It should take in file
+// And spit out another file that will be the assembled output
+
+func (ha *HackAssembler) assembleFile() {
+
 }
